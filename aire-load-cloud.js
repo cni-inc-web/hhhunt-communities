552aca757eb96ac1bc78f2b27d7817437bb5b89c
MHinson0348 <madison@cni-inc.com>
2024-08-28 15:30:34 -0400

    Make Aire Cloud Load script

---
+++ aire-load-cloud.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the .aire-hero_buildings element
+    var heroBuildings = document.querySelector('.aire-hero_buildings');
+
+    // Create an image element to track the loading of the background image
+    var bgImage = new Image();
+    bgImage.src = getComputedStyle(heroBuildings).backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2');
+    
+    // When the background image is fully loaded
+    bgImage.onload = function() {
+        fadeOutBackground(heroBuildings);
+    };
+
+    function fadeOutBackground(element) {
+        element.style.transition = "background-color 2s ease";
+        element.style.backgroundColor = "transparent";
+    }
+});
