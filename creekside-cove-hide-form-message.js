ec117b7cfc8498a43d3294b659952b7f6aa981c1
Computer-Nerds-Interactive <madison@cni-inc.com>
2025-05-31 23:54:06 -0400

    Update creekside-cove-hide-form-message.js

--- creekside-cove-hide-form-message.js
+++ creekside-cove-hide-form-message.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const formDone = document.querySelector('.w-form-done');
-  const targetElement = document.querySelector('.fc_form-p.fc-contact.line2.no-bg');
+  const targetElement1 = document.querySelector('.fc_form-p.fc-contact.line2.no-bg');
+  const targetElement2 = document.querySelector('.zoning-wh-click-to-enlarge.al.lighter');
 
-  if (formDone && targetElement) {
+  if (formDone && targetElement1 && targetElement2) {
     const style = window.getComputedStyle(formDone);
     if (style.display !== 'none') {
-      targetElement.style.display = 'none';
+      targetElement1.style.display = 'none';
+      targetElement2.style.display = 'none';
     }
   }
 });
